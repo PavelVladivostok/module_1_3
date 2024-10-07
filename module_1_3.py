57b941e6242c4a0af1e4f78a1340a0078dd9e438
PavelVladivostok <zihvav@gmail.com>
2024-10-07 22:08:30 +1000

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,19 @@
+# "Динамическая типизация"
+
+# моё имя
+name = "Pasha"
+print("name:",name)
+# мой возраст
+age = 26
+print ("age",age)
+# new_age
+a=15
+a=17
+age_new=(a+a)
+print("age_new:",age_new)
+# создания переменой Is Student
+
+Is_Student = False
+print (Is_Student)
+
+# спасибо за  задание я вас всех люблю )))))
